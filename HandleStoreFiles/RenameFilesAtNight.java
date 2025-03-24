9bdf3a1e1cd5b79d3b96f90b005a7bd7c115ba50
Racheli-Shtrochlitz <r0556735906@gmail.com>
2025-03-25 01:20:17 +0200

    I didnt finish night proccess alreade

---
+++ HandleStoreFiles/RenameFilesAtNight.java
@@ -0,0 +1,13 @@
+package HandleStoreFiles;
+
+import java.io.File;
+
+public class RenameFilesAtNight extends Thread{
+
+    public void RenameFiles(String dir,String text){
+        File file=new File(dir);
+        for(File f:file.listFiles()){
+            
+        }
+    }
+}
