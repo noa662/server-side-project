0929b36155cfc4a5cb3ec68e13b0614f0e2e8c1c
Racheli-Shtrochlitz <r0556735906@gmail.com>
2025-03-26 01:00:59 +0200

    change InquiryManager to immplement singleton design pattern

--- business/InquiryManager.java
+++ business/InquiryManager.java
@@ -9,11 +9,17 @@ import java.util.concurrent.*;
 
 public class InquiryManager {
 
+    private static InquiryManager instance;
     private BlockingQueue<InquiryHandling> q;
     private ExecutorService executer;
     private boolean running=true;
 
-    public InquiryManager() {
+    public static InquiryManager getInstance(){
+        if(instance==null)
+            instance=new InquiryManager();
+        return instance;
+    }
+    private InquiryManager() {
        q = new LinkedBlockingQueue<>();
        executer= Executors.newCachedThreadPool();
        start();//הפעלה של הפונקציה לשליפה מהתור בסרד נפרד ע"י הפונקציה start
@@ -91,59 +97,3 @@ public void inquiryCreation() {
         }
     }
 }
-
-
-
-//    private Queue<InquiryHandling> q;
-//
-//    public InquiryManager() {
-//        q = new LinkedList<>();
-//    }
-
-//    public void inquiryCreation() {
-//        Scanner scanner = new Scanner(System.in);
-//        String choose;
-//        String description;
-//        while (true) {
-//            System.out.println("enter your choose, 1->Question 2->Request 3->Complaint");
-//            choose = scanner.next();
-//            switch (choose) {
-//                case "1": {
-//                    System.out.println("Add a short description");
-//                    description = scanner.next();
-//                    q.add(new InquiryHandling(new Question(description)));
-//                    break;
-//                }
-//                case "2": {
-//                    System.out.println("Add a short description");
-//                    description = scanner.next();
-//                    q.add(new InquiryHandling(new Request(description)));
-//                    break;
-//                }
-//                case "3": {
-//                    System.out.println("Add a short description");
-//                    description = scanner.next();
-//                    System.out.println("Insert the assigned branch");
-//                    String assignedBranch = scanner.next();
-//                    q.add(new InquiryHandling(new Complaint(description,assignedBranch)));
-//                    break;
-//                }
-//                default: {
-//                    if (!choose.equals("exit")){
-//                        System.out.println("Unvalid input, try again");
-//                        continue;
-//                    }
-//                    break;
-//                }
-//            }
-//            if (choose.equals("exit"))
-//                break;
-//        }
-//    }
-//
-////מנהלת את תור הפניות
-//public void processInquiryManager() {
-//    while (!q.isEmpty())
-//        q.poll().start();
-//}
-//}
