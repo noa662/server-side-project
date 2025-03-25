23cdcb8077fbc516e216b2a46483458ee2e4ec98
Racheli-Shtrochlitz <r0556735906@gmail.com>
2025-03-25 22:23:56 +0200

    HandleFiles complited

--- HandleStoreFiles/RenameFilesAtNight.java
+++ HandleStoreFiles/RenameFilesAtNight.java
@@ -1,13 +1,37 @@
 package HandleStoreFiles;
 
 import java.io.File;
+import java.util.List;
 
 public class RenameFilesAtNight extends Thread{
 
-    public void RenameFiles(String dir,String text){
-        File file=new File(dir);
-        for(File f:file.listFiles()){
-            
+    String directory;
+    String text;
+
+    public RenameFilesAtNight(String directory,String text){
+        this.directory=directory;
+        this.text=text;
+    }
+
+    @Override
+    public void run() {
+        File dir=new File(directory);
+        if (!dir.exists() || !dir.isDirectory()) {
+            System.out.println(" error: The directory doesn't exist" + directory);
+            return;
+        }
+
+        File[] files = dir.listFiles();
+        if (files == null) {
+            System.out.println("error:  There is no files in the directory " + directory);
+            return;
+        }
+
+        for(File f:files){
+            if(f.isFile()){
+                f.renameTo(new File(directory, text + f.getName()));
+                System.out.println("Rename file to "+f.getName());
+            }
         }
     }
 }
